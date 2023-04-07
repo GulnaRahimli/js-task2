944e34de0dfea720bfc156d0eddeed68fff661e8
Gulnar <124705205+GulnaRahimli@users.noreply.github.com>
2023-04-07 17:12:10 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+
+// !! task-1 (month)
+
+// const month = prompt ("tarixi daxil edin")
+
+
+// switch (month)
+
+// {
+// case "1": console.log("Yanvar");
+// break;
+// case "2": console.log("Fevral");
+// break;
+// case "3": console.log("Mart");
+// break;
+// case "4": console.log("Aprel");
+// break;
+// case "5": console.log("May");
+// break;
+// case "6": console.log("Iyun");
+// break;
+// case "7": console.log("Iyul");
+// break;
+// case "8": console.log("Avgust");
+// break;
+// case "9": console.log("Sentyabr");
+// break;
+// case "10": console.log("Oktyabr");
+// break;
+// case "11": console.log("Noyabr");
+// break;
+// case "12": console.log("Dekabr");
+// break;
+// default : console.log("bele ay movcud deyil");
+// }
+
+// !! ======================= task - 2 ==============================
+
+
+// const model = prompt ( "Markani daxil edin");
+
+// switch (model) 
+// {
+// case "BMW": console.log("year-2019, color-black distance-198000km");
+// break ;
+
+// case "Mercedes": console.log("year-2018, color-blue , distance-1877367km");
+// break ;
+
+// case "Audi": console.log("year-2015, color-white , distance-1797673km");
+// break ;
+
+// case "Range Rover" : console.log(" year - 2023 , color-black, distance-0km");
+
+// default: console.log("bu avtomobil movcud deyil");
+
+// }
+
+// !! 
+
+const carName = prompt("markani daxil edin");
+const year = prompt("ili daxil edin")
+const data = {
+    carName: carName,
+    year: year,
+};
+
+console.log(data);
